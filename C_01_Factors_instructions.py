4a921ea9a3224332773d09f3ff3e8c749a69f2ab
Yoda-Yoda-Yoda <chambersl3255@masseyhigh.school.nz>
2024-08-01 09:44:27 +1200

    Working instructions

--- C_01_Factors_instructions.py
+++ C_01_Factors_instructions.py
@@ -7,10 +7,15 @@ def instructions():
     statement_generator("Details and Instructions", "-")
 
     print('''
-Instructions go here
-- instruction 1
-- instruction 2
-- etc
+To use this program simply enter an integer between 
+1 and 200.  The program will show the factors of your
+chosen integer.
+
+it will also tell you if your chosen number...
+- is a prime number (ie: it has two factors)
+- is a perfect square
+
+To exit the program, please type 'xxx' 
     ''')
 
 
